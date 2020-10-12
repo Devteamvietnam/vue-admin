64f839c63cbb9ca4d7f405ba3dc27dc7f15275cb
Ivanluca <jungjihoonkr97@gmail.com>
2020-10-12 17:31:39 +0700

    config locales

---
+++ src/locales/i18n.js
@@ -0,0 +1,145 @@
+module.exports = {
+  messages: {
+    VI: {
+      home: { name: 'Trang Chủ' },
+      dashboard: {
+        name: 'Dashboard',
+        workplace: { name: 'Workbench' },
+        analysis: { name: 'Analysis Page' }
+      },
+      form: {
+        name: 'Form page',
+        basic: { name: 'Basic form' },
+        step: { name: 'step-by-step form' },
+        advance: { name: 'step-by-step form' }
+      },
+      list: {
+        name: 'List page',
+        query: { name: 'query form' },
+        primary: { name: 'standard list' },
+        card: { name: 'Card List' },
+        search: {
+          name: 'Search list',
+          article: { name: 'article' },
+          application: { name: 'application' },
+          project: { name: 'Project' }
+        }
+      },
+      details: {
+        name: 'Detail page',
+        basic: { name: 'Basic Details Page' },
+        advance: { name: 'Advanced Details Page' }
+      },
+      result: {
+        name: 'Result page',
+        success: { name: 'success' },
+        error: { name: 'Failed' }
+      },
+      exception: {
+        name: 'Exception page',
+        404: { name: '404' },
+        403: { name: '403' },
+        500: { name: '500' }
+      },
+      components: {
+        name: 'Widget',
+        taskCard: { name: 'Task Card' },
+        palette: { name: 'color checkbox' }
+      }
+    },
+    US: {
+      home: { name: 'Home' },
+      dashboard: {
+        name: 'Dashboard',
+        workplace: { name: 'Workbench' },
+        analysis: { name: 'Analysis Page' }
+      },
+      form: {
+        name: 'Form page',
+        basic: { name: 'Basic form' },
+        step: { name: 'step-by-step form' },
+        advance: { name: 'step-by-step form' }
+      },
+      list: {
+        name: 'List page',
+        query: { name: 'query form' },
+        primary: { name: 'standard list' },
+        card: { name: 'Card List' },
+        search: {
+          name: 'Search list',
+          article: { name: 'article' },
+          application: { name: 'application' },
+          project: { name: 'Project' }
+        }
+      },
+      details: {
+        name: 'Detail page',
+        basic: { name: 'Basic Details Page' },
+        advance: { name: 'Advanced Details Page' }
+      },
+      result: {
+        name: 'Result page',
+        success: { name: 'success' },
+        error: { name: 'Failed' }
+      },
+      exception: {
+        name: 'Exception page',
+        404: { name: '404' },
+        403: { name: '403' },
+        500: { name: '500' }
+      },
+      components: {
+        name: 'Widget',
+        taskCard: { name: 'Task Card' },
+        palette: { name: 'color checkbox' }
+      }
+    },
+    KR: {
+      집: { 이름: '집' },
+      대시보드: {
+        name: '대시 보드',
+        직장: { 이름: 'Workbench' },
+        분석: { 이름: '분석 페이지' }
+      },
+      형식: {
+        name: '양식 페이지',
+        기본: { 이름: '기본 형식' },
+        단계: { 이름: '단계별 양식' },
+        사전: { 이름: '단계별 양식' }
+      },
+      목록: {
+        name: '목록 페이지',
+        쿼리: { name: 'query form' },
+        기본: { name: 'standard list' },
+        카드: { 이름: '카드 목록' },
+        검색: {
+          name: '검색 목록',
+          기사: { name: 'article' },
+          애플리케이션: { name: 'application' },
+          프로젝트: { name: 'Project' }
+        }
+      },
+      세부정보: {
+        name: '상세 페이지',
+        기본: { 이름: '기본 세부 정보 페이지' },
+        사전: { 이름: '고급 세부 정보 페이지' }
+      },
+      결과: {
+        name: '결과 페이지',
+        성공: { 이름: 'success' },
+        오류: { name: 'Failed' }
+      },
+      예외: {
+        이름: '예외 페이지',
+        404: { 이름: '404' },
+        403: { 이름: '403' },
+        500: { 이름: '500' }
+      },
+      구성요소: {
+        name: '위젯',
+        taskCard: { 이름: '작업 카드' },
+        팔레트: { 이름: '색상 체크 박스' }
+      }
+    }
+  }
+}
---
+++ src/plugins/i18n-extend.js
@@ -0,0 +1,32 @@
+// Statement mode
+const MODE = {
+  STATEMENTS: 's', //Statement mode
+  PHRASAL: 'p' //phrase mode
+}
+
+const VueI18nPlugin = {
+  install: function(Vue) {
+    Vue.mixin({
+      methods: {
+        $ta(syntaxKey, mode) {
+          let _mode = mode || MODE.STATEMENTS
+          let keys = syntaxKey.split('|')
+          let _this = this
+          let locale = this.$i18n.locale
+          let message = ''
+          let splitter = locale == 'US' ? '' : ''
+          // splicing message
+          keys.forEach(key => {
+            message += _this.$t(key) + splitter
+          })
+          // In the English environment sentence mode, change the case of words
+          if (keys.length > 0 && _mode == MODE.STATEMENTS && locale == 'US') {
+            message = message.charAt(0).toUpperCase() + message.toLowerCase().substring(1)
+          }
+          return message
+        }
+      }
+    })
+  }
+}
+export default VueI18nPlugin
