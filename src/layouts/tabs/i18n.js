949fa8157a2d86ce41436416309ce4a603f3708c
Ivanluca <jungjihoonkr97@gmail.com>
2020-10-14 16:51:39 +0700

    add: config & add layout

---
+++ src/layouts/tabs/i18n.js
@@ -0,0 +1,25 @@
+module.exports = {
+  messages: {
+    KO: {
+      closeLeft: '왼쪽 닫기',
+      closeRight: '오른쪽 닫기',
+      closeOthers: '기타 닫기',
+      refresh: '페이지 새로 고침',
+      warn: '이 페이지는 마지막 페이지입니다. 닫을 수 없습니다.'
+    },
+    VI: {
+      closeLeft: 'đóng bên trái',
+      closeRight: 'đóng bên phải',
+      closeOthers: 'đóng những người khác',
+      refresh: 'làm mới trang',
+      warn: 'Đây là trang cuối cùng, bạn không thể đóng nó'
+    },
+    US: {
+      closeLeft: 'close left',
+      closeRight: 'close right',
+      closeOthers: 'close others',
+      refresh: 'refresh the page',
+      warn: "This is the last page, you can't close it"
+    }
+  }
+}
