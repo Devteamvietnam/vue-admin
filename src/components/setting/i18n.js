7d971717ef3e8467c55691cefb1374af59c5a381
Ivanluca <jungjihoonkr97@gmail.com>
2020-10-15 10:30:47 +0700

    config: Config component, Add: Add component

---
+++ src/components/setting/i18n.js
@@ -0,0 +1,120 @@
+module.exports = {
+  messages: {
+    US: {
+      theme: {
+        title: 'Page Style Setting',
+        light: 'Light Style',
+        dark: 'Dark Style',
+        night: 'Night Style',
+        color: 'Theme Color'
+      },
+      navigate: {
+        title: 'Navigation Mode',
+        side: 'Side Menu Layout',
+        head: 'Top Menu Layout',
+        mix: 'Mix Menu Layout',
+        content: {
+          title: 'Content Width',
+          fluid: 'Fluid',
+          fixed: 'Fixed'
+        },
+        fixedHeader: 'Fixed Header',
+        fixedSideBar: 'Fixed SideBar'
+      },
+      other: {
+        title: 'Other Setting',
+        weekMode: 'Week Mode',
+        multiPages: 'Multi Pages',
+        hideSetting: 'Hide Setting Drawer'
+      },
+      animate: {
+        title: 'Page Toggle Animation',
+        disable: 'Disable',
+        effect: 'Effect',
+        direction: 'Direction'
+      },
+      alert:
+        'After copying the configuration code, directly cover all contents in the file src/config/config.js, then restart the server. (Note: only items that are different from the default configuration will be copied)',
+      copy: 'Copy Setting',
+      save: 'Save',
+      reset: 'Reset'
+    },
+    VI: {
+      theme: {
+        title: 'Cài đặt Kiểu Trang',
+        light: 'Phong cách ánh sáng',
+        dark: 'Kiểu tối',
+        color: 'Màu chủ đề'
+      },
+      navigate: {
+        title: 'Chế độ điều hướng',
+        side: 'Bố cục Menu bên',
+        head: 'Bố cục Menu trên cùng',
+        mix: 'Mix Menu Layout',
+        content: {
+          title: 'Chiều rộng nội dung',
+          fluid: 'Chất lỏng',
+          fixed: 'Đã sửa'
+        },
+        fixedHeader: 'Tiêu đề cố định',
+        fixedSideBar: 'Thanh bên cố định'
+      },
+      other: {
+        title: 'Cài đặt Khác',
+        weekMode: 'Chế độ Tuần',
+        multiPages: 'Nhiều Trang',
+        hideSetting: 'Ẩn Ngăn kéo Cài đặt'
+      },
+      animate: {
+        title: 'Hoạt ảnh chuyển đổi trang',
+        disable: 'Vô hiệu hoá',
+        effect: 'Hiệu ứng',
+        direction: 'Hướng'
+      },
+      alert:
+        'Sau khi sao chép mã cấu hình, trực tiếp bao gồm tất cả nội dung trong tệp src / config / config.js, sau đó khởi động lại máy chủ. (Lưu ý: chỉ những mục khác với cấu hình mặc định mới được sao chép) ',
+      copy: 'Sao chép Cài đặt',
+      save: 'Save',
+      reset: 'Reset'
+    },
+    KO: {
+      theme: {
+        title: '페이지 스타일 설정',
+        light: 'Light Style',
+        dark: '다크 스타일',
+        night: '나이트 스타일',
+        color: '테마 색상'
+      },
+      navigate: {
+        title: '내비게이션 모드',
+        side: '사이드 메뉴 레이아웃',
+        head: '상단 메뉴 레이아웃',
+        mix: '믹스 메뉴 레이아웃',
+        content: {
+          title: '콘텐츠 너비',
+          fluid: '유체',
+          fixed: '고정'
+        },
+        fixedHeader: '고정 헤더',
+        fixedSideBar: '고정 사이드 바'
+      },
+      other: {
+        title: '기타 설정',
+        weekMode: '주 모드',
+        multiPages: '다중 페이지',
+        hideSetting: '설정 창 숨기기'
+      },
+      animate: {
+        title: '페이지 전환 애니메이션',
+        disable: '비활성화',
+        effect: '효과',
+        direction: '방향'
+      },
+      alert:
+        '구성 코드를 복사 한 후 src / config / config.js 파일의 모든 내용을 직접 덮은 다음 서버를 다시 시작하십시오. (참고 : 기본 구성과 다른 항목 만 복사됩니다.) ',
+      copy: '복사 설정',
+      save: '저장',
+      reset: '재설정'
+    }
+  }
+}
