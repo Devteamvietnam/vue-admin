98aa4b75236b1c010e1c17d97315bc634f4f68f9
Ivanluca <jungjihoonkr97@gmail.com>
2020-11-10 17:14:40 +0700

    lucas, update (fix repon table, integration package element ui), 11/10

--- src/pages/admin/list/table/List.vue
+++ src/pages/admin/list/table/List.vue
@@ -1,79 +1,113 @@
 <template>
-<a-card>
-    <a-table bordered :row-selection="rowSelection" :columns="columns" :data-source="data" :record="data.id">
-    </a-table>
-</a-card>
+<div class="app-container">
+    <a-card>
+        <el-form :model="queryParams" ref="queryForm" :inline="true" v-show="showSearch" label-width="68px">
+            <el-form-item>
+                <el-button type="cyan" icon="el-icon-search" size="mini" @click="handleQuery">Search</el-button>
+                <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">Reset</el-button>
+            </el-form-item>
+        </el-form>
+        <el-row :gutter="12" class="mb8">
+            <el-col :span="1.5">
+                <el-button type="primary" icon="el-icon-plus" size="mini">New</el-button>
+            </el-col>
+            <el-col :span="1.5">
+                <el-button type="success" icon="el-icon-edit" size="mini" :disabled="single">Edit</el-button>
+            </el-col>
+            <el-col :span="1.5">
+                <el-button type="danger" icon="el-icon-delete" size="mini" :disabled="multiple">Delete</el-button>
+            </el-col>
+        </el-row>
+
+        <el-table v-loading="loading" :data="postList" @selection-change="handleSelectionChange">
+            <el-table-column type="selection" width="55" align="center" />
+            <el-table-column sortable label="Number" align="center" prop="postId" />
+            <el-table-column label="Code" align="center" prop="postCode" />
+            <el-table-column label="Name" align="center" prop="postName" />
+            <el-table-column label="Sorting" align="center" prop="postSort" />
+            <el-table-column label="Status" align="center" prop="status" />
+            <el-table-column label="Create time" align="center" prop="createTime" width="180">
+            </el-table-column>
+            <el-table-column label="operation" align="center" class-name="small-padding fixed-width">
+                <template slot-scope="scope">
+                    <el-button size="mini" type="text" icon="el-icon-edit" @click="handleUpdate(scope.row)">Update</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <el-pagination v-show="total>0" :total="total" :page.sync="queryParams.pageNum" :limit.sync="queryParams.pageSize" />
+    </a-card>
+</div>
 </template>
 
 <script>
-const columns = [{
-        title: 'Name',
-        dataIndex: 'name',
-        sorter: true,
-        width: 200
-    },
-    {
-        title: 'Age',
-        dataIndex: 'age',
-        sorter: true,
-        width: 200
-    },
-    {
-        title: 'Address',
-        dataIndex: 'address',
-        sorter: true,
-    }
-]
-
 export default {
-    name: 'Table',
     data() {
         return {
-            apiData: [{
-                    id: '1',
-                    name: 'IvanLucas',
-                    age: 'xx',
-                    address: 'HCMC',
+            loading: false,
+            total: 0,
+            showSearch: true,
+            single: true,
+            multiple: true,
+            apiPostList: [{
+                    postId: '1',
+                    postCode: 'devteam',
+                    postName: 'Devteam',
+                    postSort: '1',
+                    status: 'normal',
+                    createTime: '2020-11-10',
                 },
                 {
-                    id: '2',
-                    name: 'IvanLucas',
-                    age: 'xx',
-                    address: 'HCMC',
-                },
+                    postId: '2',
+                    postCode: '007',
+                    postName: 'Devteam',
+                    postSort: '2',
+                    status: 'quality',
+                    createTime: '2020-11-10',
+                }
             ],
-            data: [],
-            columns,
-            selectedRowId: [], // Check here to configure the default column
+            postList: [],
+            queryParams: {
+                pageNum: 1,
+                pageSize: 10,
+                postCode: undefined,
+                postName: undefined,
+                status: undefined
+            },
         }
     },
-    computed: {
-        rowSelection() {
-            const {
-                selectedRowId
-            } = this
-            return {
-                selectedRowId,
-                onChange: this.onSelectChange,
-                hideDefaultSelections: true,
-                onSelection: this.onSelection,
-            }
-        },
-    },
     methods: {
-        onSelectChange(selectedRowId) {
-            this.selectedRowId = selectedRowId
+        cancel() {
+            this.open = false
+            this.reset()
         },
-        onDelete(id) {
-            const data = [...this.data]
-            this.data = data.filter((item) => item.id !== id)
+        reset() {
+            this.form = {
+                postId: undefined,
+                postCode: undefined,
+                postName: undefined,
+                postSort: 0,
+                status: "0",
+                remark: undefined
+            };
+            this.resetForm("form")
         },
+        handleQuery() {
+            this.queryParams.pageNum = 1
+        },
+        handleSelectionChange(selection) {
+            this.ids = selection.map(item => item.postId)
+            this.single = selection.length != 1
+            this.multiple = !selection.length
+        },
+        resetQuery() {
+            this.resetForm("queryForm");
+            this.handleQuery();
+        }
     },
     created() {
-        this.apiData.forEach((list) => {
-            list = Object.assign({}, list)
-            this.data.push(list)
+        this.apiPostList.forEach((list) => {
+            this.postList.push(list)
         })
-    },
+    }
 }
 </script>
