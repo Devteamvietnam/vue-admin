b683da170a05a1ee97c6ed13306e2ed4f377f973
Ivanluca <jungjihoonkr97@gmail.com>
2020-11-11 10:54:24 +0700

    lucas,fix table, 11/11

--- src/main.js
+++ src/main.js
@@ -9,8 +9,6 @@ import 'animate.css/source/animate.css'
 import Plugins from '@/plugins'
 import {initI18n} from '@/utils/i18n'
 import bootstrap from '@/bootstrap'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 
 const router = initRouter(store.state.setting.asyncRoutes)
 const i18n = initI18n('US')
@@ -19,7 +17,7 @@ Vue.use(Antd)
 Vue.config.productionTip = false
 Vue.use(Viser)
 Vue.use(Plugins)
-Vue.use(ElementUI)
+
 
 bootstrap({
   router,
